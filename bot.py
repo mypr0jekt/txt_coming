9632c6176cf7ca677d0d2ca1e28a3af126844d40
Saidabbos <abbosxojavaqqosov@gmail.com>
2026-07-27 20:25:06 +0500

    Update BOT_TOKEN in bot.py
    Replaced placeholder BOT_TOKEN with actual token.

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ import db
 logging.basicConfig(level=logging.INFO)
 
 # ---------- SOZLAMALAR ----------
-BOT_TOKEN = os.getenv("BOT_TOKEN", "SIZNING_BOT_TOKEN_BU_YERGA")
+BOT_TOKEN = os.getenv("8948791881:AAHWiUQWqmNBuUrhV5Cc8HUd73ro7YR7sXw")
 # Birinchi (bosh) admin - botni ishga tushirgan odam. Bu ID doim admin bo'lib qoladi.
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
